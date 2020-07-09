f359e172e5b5acb330e2f30a1ae0787ae4e6f4f5
Aixa <aixa.lacroix@gmail.com>
2020-07-09 11:00:29 -0400

    first commit

---
+++ calc.py
@@ -0,0 +1,14 @@
+# Three step process to save to Github
+# 1. Add the files that you'd want to save
+
+git add [filename]
+OR 
+git add . #adds all files
+
+# 2. Name this saved version
+git commit -m "name"
+
+# 3. Push the save to github
+git push
+
+print("Hello world")
